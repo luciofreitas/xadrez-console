2f6ea1715276ca44042cec7af83f151205f1fa8c
luciofreitas <luciodfp@gmail.com>
2023-05-02 15:12:39 -0300

    ajustando o nome e a sintaxe dos atributos das classes

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -8,17 +8,17 @@ namespace xadrez_console.tabuleiro
 {
     internal class Peca
     {
-        public Posicao Posicao { get; set; }
-        public Cor Cor { get; protected set; }
-        public int QuantidadeMovimentos { get; protected set; }
-        public Tabuleiro Tabuleiro { get; protected set; }
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int quantidadeMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
 
         public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
         {
-            Posicao = posicao;
-            Cor = cor;
-            Tabuleiro = tabuleiro;
-            this.QuantidadeMovimentos = 0;
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.quantidadeMovimentos = 0;
         }
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -10,7 +10,7 @@ namespace xadrez_console.tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        private Peca[,] Pecas { get; set; }
+        private Peca[,] Pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
